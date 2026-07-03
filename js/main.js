7485bcfc3a6b006ad1dc0df6bd139adf30a3d768
daikisato1025 <20234068-satodaiki@hcs.ac.jp>
2026-07-03 13:51:52 +0900

    前回入力修正

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 let editingId = null;
 
-document.addEventListener("DOMContentLoaded", function () {
-
-const last = JSON.parse(localStorage.getItem("lastStudyInput") || "{}");
-
-if (last.subject) {
-    document.getElementById("subject").value = last.subject;
-}
-
-if (last.studyType) {
-    el.value = last.studyType;
-    el.dispatchEvent(new Event("change"));
-}
-
+document.addEventListener("DOMContentLoaded", () => {
+    restoreLastInput();
 });
+
+function restoreLastInput() {
+
+    const last = JSON.parse(localStorage.getItem("lastStudyInput") || "{}");
+
+    if (last.subject) {
+        document.getElementById("subject").value = last.subject;
+    }
+
+    if (last.studyType) {
+        document.getElementById("studyType").value = last.studyType;
+    }
+}
